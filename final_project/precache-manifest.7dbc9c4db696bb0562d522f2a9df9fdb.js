50843683a10d00db7240e74ae469f6837e42cc72
bwu1 <bofan94@hotmail.com>
2021-11-24 19:45:44 -0500

    finishing touches fix directory error

---
+++ final_project/precache-manifest.7dbc9c4db696bb0562d522f2a9df9fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8506c868c6a9ae8a1dc",
+    "url": "/final_project/static/css/main.bd97043c.chunk.css"
+  },
+  {
+    "revision": "e8506c868c6a9ae8a1dc",
+    "url": "/final_project/static/js/main.e8506c86.chunk.js"
+  },
+  {
+    "revision": "d82c4410d66bd8c15a23",
+    "url": "/final_project/static/css/1.56bf7600.chunk.css"
+  },
+  {
+    "revision": "d82c4410d66bd8c15a23",
+    "url": "/final_project/static/js/1.d82c4410.chunk.js"
+  },
+  {
+    "revision": "4aeed05ce8af3624a08c",
+    "url": "/final_project/static/js/runtime~main.4aeed05c.js"
+  },
+  {
+    "revision": "3ee41bff7d276524751f5a18fb865921",
+    "url": "/final_project/index.html"
+  }
+];
--- final_project/service-worker.js
+++ final_project/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/final_proect/precache-manifest.a9ef5d98be3dbbf4cadb811461cb9852.js"
+  "/final_project/precache-manifest.7dbc9c4db696bb0562d522f2a9df9fdb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/final_proect/index.html", {
+workbox.routing.registerNavigationRoute("/final_project/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
