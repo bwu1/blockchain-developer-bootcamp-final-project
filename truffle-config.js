3d4414fc1f0c83141c51862fddc918336f7b5ccd
bwu1 <bofan94@hotmail.com>
2021-11-24 20:34:06 -0500

    final commit

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+//const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 /*
 //Ropsten Settings
